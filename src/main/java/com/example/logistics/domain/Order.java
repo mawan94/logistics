4f96edddcbc8a17bc555ab1db274eee75f51c67a
Long-Liu <1263601750@qq.com>
2020-05-12 18:21:04 +0800

    fix

--- src/main/java/com/example/logistics/domain/Order.java
+++ src/main/java/com/example/logistics/domain/Order.java
@@ -5,7 +5,11 @@ import lombok.Data;
 @Data
 public class Order {
     private String id;
+    private String recipientId;
     private String customerId;
     private String staffId;
     private int status;
+    private String recipientAddress;
+    private String recipientPhoneNumber;
+    private String courierFee;
 }
